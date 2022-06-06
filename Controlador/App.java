03ef9bf70ae61a8ef7c01d9e5eda11963d9b8a40
nash23234 <hengerlynnash26@gmail.com>
2022-06-05 20:52:51 -0600

    Finalizacion del proyecto

---
+++ Controlador/App.java
@@ -0,0 +1,12 @@
+package Controlador;
+
+/**
+ * @author Hengerlyn_Nash
+ */
+
+public class App {
+    public static void main(String[] args) {
+        Control control = new Control();
+
+    }
+}
